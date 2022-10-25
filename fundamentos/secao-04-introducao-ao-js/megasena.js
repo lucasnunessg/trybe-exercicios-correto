b4948094f0ef56929d3403a2441b66a7d0337b5e
lucasnunessg <lucasnunespacheco@gmail.com>
2022-10-25 15:50:17 -0300

    sorteando e organizando a mega sena

--- fundamentos/secao-04-introducao-ao-js/megasena.js
+++ fundamentos/secao-04-introducao-ao-js/megasena.js
@@ -1,7 +1,7 @@
 // jogo lucas
 
 let jogoLucas = [13, 17, 24, 39, 42, 59];
-console.log (jogoLucas);
+
 
 // Criando o sorteio
 let n1 = Math.floor (Math.random() * 60) +1;
@@ -23,3 +23,21 @@ for (let index = 0; index < jogoLucas.length;index +=1) {
 for (let index = 0; index < megaSena.length; index += 1) {
     console.log (megaSena[index]);
 }
+
+// 4- Comparando o jogo com o sorteio.
+
+let acertos = 0;
+
+  for (let iJogo = 0; iJogo < jogoLucas.length;iJogo +=1) {
+   
+  for (let iSorteio = 0; iSorteio < megaSena.length; iSorteio += 1) {
+  
+    if (jogoLucas [iJogo] == megaSena [iSorteio]) {
+        acertos +=1;
+    }
+}
+}
+
+// 5 - Imprimindo os resultados
+
+console.log ('Número de acertos: ', acertos)
