e35eb29d39f7d4c3e7272059a96286c51bf80294
lucasnunessg <lucasnunespacheco@gmail.com>
2022-10-25 09:22:34 -0300

    praticando

--- fundamentos/secao-04-introducao-ao-js/testejs.js
+++ fundamentos/secao-04-introducao-ao-js/testejs.js
@@ -1,16 +1,17 @@
 let fralda;
-let tamanhoFralda = 5
+let tamanhoFralda = 6;
 
 if (tamanhoFralda <= 4) {
     fralda = 'RN'
-} else if (tamanhoFralda > 6 && tamanhoFralda <=8 ) {
+} else if (tamanhoFralda > 4 && tamanhoFralda <=6 ) {
     fralda = 'Pqn'
 } else if (tamanhoFralda > 8 && tamanhoFralda <=10) {
     fralda = 'Med'
 } else if (tamanhoFralda > 12 && tamanhoFralda <=14) {
-    fralda = 'g'
+    fralda = 'G'
 } else if (tamanhoFralda > 16 && tamanhoFralda <=18 ) {
-    fralda = 'gg'
+    fralda = 'GG'
 } else if (tamanhoFralda > 18) {
     fralda = 'Parabéns, ele cresceu!'
 }
+console.log (fralda)
